a6656499024b6e31bc840c6a1fddb780dd9f9459
Eric Camescasse <eric.camescasse@flashparking.com>
2024-04-11 17:42:48 -0400

    refactor with test inheritance

---
+++ src/test/java/BaseTests.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public abstract class BaseTests {
+    protected ChromeDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
--- src/test/java/StudentFormTests.java
+++ src/test/java/StudentFormTests.java
@@ -1,37 +1,22 @@
 import org.example.pageObjectModels.ConfirmationModal;
 import org.example.pageObjectModels.StudentForm;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static com.google.common.truth.Truth.assertThat;
 
-public class StudentFormTests {
-
-    private ChromeDriver driver;
-
-    @BeforeMethod
-    public void setUp() {
-        driver = new ChromeDriver();
-    }
+public class StudentFormTests extends BaseTests {
 
     @Test
     public void studentForm_inputsRequiredValues_createsUser() throws InterruptedException {
-        var form = new StudentForm(driver);
-        form.open()
-            .setName("Eric", "Camescasse")
-            .setGender("Male")
-            .setMobile("8095980728")
-            .submit();
+        new StudentForm(driver)
+                .open()
+                .setName("Eric", "Camescasse")
+                .setGender("Male")
+                .setMobile("8095980728")
+                .submit();
 
         var modal = new ConfirmationModal(driver);
 
         assertThat(modal.getTitleText()).isEqualTo("Thanks for submitting the form");
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        driver.quit();
     }
 }
