347340dafff1d49fa39c445851425e2d26af3219
Eric Camescasse <e.camescasse@gmail.com>
2024-04-15 14:31:04 -0400

    github actions test

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -7,7 +7,8 @@ public abstract class BaseTest {
     protected WebDriver driver;
 
     @BeforeMethod
-    public void setUp() throws Exception {
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
         driver = PropertiesReader.getInstance().getDriver();
     }
 
