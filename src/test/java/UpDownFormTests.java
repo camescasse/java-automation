5ac2caf9d9fb8f27b0418e13fb7e970c21739a8f
camescasse <e.camescasse@gmail.com>
2024-04-17 14:18:15 -0400

    fixed file path compatibility

--- src/test/java/UpDownFormTests.java
+++ src/test/java/UpDownFormTests.java
@@ -13,8 +13,16 @@ public class UpDownFormTests extends BaseTest {
     }
 
     @Test
-    public void upDownForm_uploadsFile_FormShowsFileName() {
+    public void upDownForm_uploadsDoc_FormShowsFileName() {
         var fileName = "sample-doc.txt";
+        var form = new upDownForm(driver).open().uploadFile(fileName);
+
+        assertThat(form.isUploaded()).isTrue();
+    }
+
+    @Test
+    public void upDownForm_uploadsImage_FormShowsFileName() {
+        var fileName = "sample-image.jpeg";
         var form = new upDownForm(driver).open().uploadFile(fileName);
 
         assertThat(form.isUploaded()).isTrue();
